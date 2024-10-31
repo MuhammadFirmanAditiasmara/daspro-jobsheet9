9c8521333d9f9d8a0bcff6eaef20afdfc1023152
Adidtiasmara <mfirmanadid@gmail.com>
2024-10-31 10:58:58 +0700

    Percobaan 1

--- ArrayBilangan18.java
+++ ArrayBilangan18.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 /**
@@ -8,16 +9,16 @@ public class ArrayBilangan18 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int bil [] = new int[4];
+        int bil[] = new int[4];
 
         bil[0] = 5;
         bil[1] = 13;
         bil[2] = -7;
         bil[3] = 17;
 
-        for (int i = 0; i < 4; i++) {
-            System.out.println(bil[i]);
-            
-        }
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
     }
 }
