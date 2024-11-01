714b727820eaaa0d0c562a83eb12ba5b4ae1f7b8
Adidtiasmara <mfirmanadid@gmail.com>
2024-11-01 07:44:05 +0700

    Percobaan 4

--- SearchNilai18.java
+++ SearchNilai18.java
@@ -1,19 +1,47 @@
 
+import java.util.Scanner;
+
 public class SearchNilai18 {
 
     public static void main(String[] args) {
-        int arrNilai[] = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner input = new Scanner(System.in);
+        // int arrNilai[] = {80, 85, 78, 96, 90, 82, 86};
+        // int key = 90;
 
-        for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
-                hasil = i;
+        // for (int i = 0; i < arrNilai.length; i++) {
+        // if (key == arrNilai[i]) {
+        // hasil = i;
+        // break;
+        // }
+        // }
+        System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+        int jumlah = input.nextInt();
+
+        int nilai[] = new int[jumlah];
+
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilai[i] = input.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        int key = input.nextInt();
+
+        int hasil = 0;
+        for (int i = 0; i < jumlah; i++) {
+
+            if (key == nilai[i]) {
+                hasil = i + 1;
                 break;
+
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        if (hasil != 0) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil));
+
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
+
+        input.close();
     }
 }
